27404d3712fa3977dec5670707f756a1f4e20d1e
haochuan ding <haochuan.ding@kronos.com>
2017-11-27 19:38:03 -0500

    Include contributed modules: 1. rm -rf modules/MMM-Scrobbler/.git 2. rm -rf modules/MMM-MBTA/.git 3. comment out git ignore modules for /modules/default, now only ignore node_modules

---
+++ modules/MMM-MBTA/MMM-MBTA.js
@@ -0,0 +1,530 @@
+Module.register("MMM-MBTA", {
+  defaults: {
+    apikey: "",
+    updateInterval: 20, // In seconds
+    baseUrl: "https://realtime.mbta.com/developer/api/v2/",
+    stations: ["Northeastern University Station"],
+    doAnimation: false,
+    animationSpeed: 1000,
+    formatETA: true,
+    showMinutesOnly: false,
+    showOnly: [],
+    maxEntries: 8,
+    maxTime: 0,
+    showArrivalTime: false,
+    showETATime: true,
+    fade: true,
+    fadePoint: 0.25, // Start on 1/4th of the list.
+    showFullName: false,
+    showAlerts: false,
+  },
+
+  getStyles: function () {
+    return ["font-awesome.css"];
+  },
+
+  getHeader: function () {
+    return this.data.header + " " + this.config.stations[0];
+  },
+
+  getScripts: function () {
+    return ["moment.js"];
+  },
+
+  start: function () {
+    // API abuse prevention
+    if (this.config.updateInterval < 10) {
+      this.config.updateInterval = 10;
+    }
+
+    this.loaded = false;
+    // Dictionary sincerely stolen from https://github.com/mbtaviz/mbtaviz.github.io/
+    // and green line dictionary data taken from https://github.com/mbtaviz/green-line-release/
+    // TODO: Get from external file
+    var stationDict = {
+      "Airport": "place-aport",
+      "Aquarium": "place-aqucl",
+      "Beachmont": "place-bmmnl",
+      "Bowdoin": "place-bomnl",
+      "Government Center": "place-gover",
+      "Maverick": "place-mvbcl",
+      "Orient Heights": "place-orhte",
+      "Revere Beach": "place-rbmnl",
+      "Suffolk Downs": "place-sdmnl",
+      "Wonderland": "place-wondl",
+      "Wood Island": "place-wimnl",
+      "Back Bay": "place-bbsta",
+      "Chinatown": "place-chncl",
+      "Community College": "place-ccmnl",
+      "Forest Hills": "place-forhl",
+      "Green Street": "place-grnst",
+      "Haymarket": "place-haecl",
+      "Jackson Square": "place-jaksn",
+      "Malden Center ": "place-mlmnl",
+      "Mass Ave": "place-masta",
+      "North Station": "place-north",
+      "Oak Grove": "place-ogmnl",
+      "Roxbury Crossing": "place-rcmnl",
+      "Ruggles": "place-rugg",
+      "Stony Brook": "place-sbmnl",
+      "Sullivan Square": "place-sull",
+      "Tufts Medical Center": "place-tumnl",
+      "Wellington ": "place-welln",
+      "State Street": "place-state",
+      "Alewife": "place-alfcl",
+      "Andrew Square": "place-andrw",
+      "Ashmont": "place-asmnl",
+      "Braintree": "place-brntn",
+      "Broadway": "place-brdwy",
+      "Central Square": "place-cntsq",
+      "Charles MGH": "place-chmnl",
+      "Davis Square": "place-davis",
+      "Fields Corner": "place-fldcr",
+      "Harvard": "place-harsq",
+      "JFK/U Mass": "place-jfk",
+      "Kendall Square": "place-knncl",
+      "North Quincy": "place-nqncy",
+      "Park Street": "place-pktrm",
+      "Porter Square": "place-portr",
+      "Quincy Adams": "place-qamnl",
+      "Quincy Center": "place-qnctr",
+      "Savin Hill": "place-shmnl",
+      "Shawmut": "place-smmnl",
+      "South Station": "place-sstat",
+      "Wollaston": "place-wlsta",
+      "Downtown Crossing": "place-dwnxg",
+      "Lechmere Station": "place-lech",
+      "Science Park Station": "place-spmnl",
+      "North Station": "place-north",
+      "Haymarket Station": "place-haecl",
+      "Government Center Station": "place-gover",
+      "Park Street": "place-pktrm",
+      "Boylston Street Station": "place-boyls",
+      "Arlington Station": "place-armnl",
+      "Copley Station": "place-coecl",
+      "Prudential Station": "place-prmnl",
+      "Symphony Station": "place-symcl",
+      "Northeastern University Station": "place-nuniv",
+      "Museum of Fine Arts Station": "place-mfa",
+      "Longwood Medical Area Station": "place-lngmd",
+      "Brigham Circle Station": "place-brmnl",
+      "Fenwood Road Station": "place-fenwd",
+      "Mission Park Station": "place-mispk",
+      "Riverway Station": "place-rvrwy",
+      "Back of the Hill Station": "place-bckhl",
+      "Heath Street Station": "place-hsmnl",
+      "Hynes Convention Center": "place-hymnl",
+      "Kenmore Station": "place-kencl",
+      "Fenway Station": "place-fenwy",
+      "Longwood Station": "place-longw",
+      "Brookline Village Station": "place-bvmnl",
+      "Brookline Hills Station": "place-brkhl",
+      "Beaconsfield Station": "place-bcnfd",
+      "Reservoir Station": "place-rsmnl",
+      "Chestnut Hill Station": "place-chhil",
+      "Newton Centre Station": "place-newto",
+      "Newton Highlands Station": "place-newtn",
+      "Eliot Station": "place-eliot",
+      "Waban Station": "place-waban",
+      "Woodland Station": "place-woodl",
+      "Riverside Station": "place-river",
+      "Saint Mary Street Station": "place-smary",
+      "Hawes Street Station": "place-hwsst",
+      "Kent Street Station": "place-kntst",
+      "Saint Paul Street": "place-stpul",
+      "Coolidge Corner Station": "place-cool",
+      "Summit Avenue Station": "place-sumav",
+      "Brandon Hall Station": "place-bndhl",
+      "Fairbanks Street Station": "place-fbkst",
+      "Washington Square Station": "place-bcnwa",
+      "Tappan Street Station": "place-tapst",
+      "Dean Road Station": "place-denrd",
+      "Englewood Avenue Station": "place-engav",
+      "Cleveland Circle Station": "place-clmnl",
+      "Blandford Street Station": "place-bland",
+      "Boston University East Station": "place-buest",
+      "Boston University Central Station": "place-bucen",
+      "Boston University West Station": "place-buwst",
+      "Pleasant Street Station": "place-plsgr",
+      "Babcock Street Station": "place-babck",
+      "Packards Corner Station": "place-brico",
+      "Harvard Avenue Station": "place-harvd",
+      "Griggs Street Station": "place-grigg",
+      "Allston Street Station": "place-alsgr",
+      "Warren Street Station": "place-wrnst",
+      "Washington Street Station": "place-wascm",
+      "Sutherland Road Station": "place-sthld",
+      "Chiswick Road Station": "place-chswk",
+      "Chestnut Hill Avenue Station": "place-chill",
+      "South Street Station": "place-sougr",
+      "Boston College Station": "place-lake",
+      "Winchester": "place-wowin"
+    };
+
+
+    // Convert colloquial names to stop ids
+    this.stations = [];
+    for (let i = 0; i < this.config.stations.length; i++) {
+      this.stations[i] = stationDict[this.config.stations[i]];
+    }
+
+    this.stationData = []; // Clear station data
+
+    this.filterModes = [];
+    if (this.config.showOnly.includes("Subway")) {
+      // Light rail and subway are synonymous in Boston
+      this.filterModes.push("0");
+      this.filterModes.push("1");
+    }
+    if (this.config.showOnly.includes("Train")) {
+      this.filterModes.push("2");
+    }
+    if (this.config.showOnly.includes("Bus")) {
+      this.filterModes.push("3");
+    }
+    if (this.config.showOnly.includes("Ferry")) {
+      this.filterModes.push("4");
+    }
+    if (this.config.showOnly.includes("Cable car")) {
+      this.filterModes.push("5");
+    }
+
+    this.alerts = [];
+
+  },
+
+  getDom: function () {
+    var wrapper = document.createElement("div");
+
+    if (!this.loaded) {
+      wrapper.innerHTML += "LOADING";
+      wrapper.className = "dimmed light small";
+    }
+
+
+    // Check if an API key is in the config
+    if (this.config.apikey === "") {
+      if (wrapper.innerHTML !== "") {
+        wrapper.innerHTML += "<br/>";
+      }
+      wrapper.innerHTML += "Please set a MBTA api key! This module won't load otherwise!";
+      wrapper.className = "dimmed light small";
+      return wrapper; // Do not continue updating
+    } else if (this.config.apikey === "wX9NwuHnZU2ToO7GmGR9uw") {
+      if (wrapper.innerHTML !== "") {
+        wrapper.innerHTML += "<br/>";
+      }
+      wrapper.innerHTML += "Warning! You are using a dev api key!";
+      wrapper.className = "dimmed light small";
+    }
+
+    /*-----------------------------------------*/
+
+    var table = document.createElement("table");
+    table.className = "small";
+
+    for (let i = 0; i < this.stationData.length; i++) {
+      var row = document.createElement("tr");
+      table.appendChild(row);
+
+      // Icon
+      var symbolCell = document.createElement("td");
+      switch (this.stationData[i].routeType) {
+        case "0": // Tram/Streetcar/Light Rail case. We'll use the same icon.
+        case "1":
+          symbolCell.className = "fa fa-subway";
+          break;
+        case "2":
+          symbolCell.className = "fa fa-train";
+          break;
+        case "3":
+          symbolCell.className = "fa fa-bus";
+          break;
+        case "4":
+          symbolCell.className = "fa fa-ship";
+          break;
+        case "5": // Suppose to be a cable car but there's no FA icon, so we'll just use the train icon
+          symbolCell.className = "fa fa-train";
+          break;
+        case "6": // Gondola case
+        // There shouldn't be a gondola in Boston.
+        case "7": // Funicular case
+        // There shouldn't be a funicular in Boston.
+        default:
+          symbolCell.className = "fa fa-question-circle-o";
+      }
+      symbolCell.style.cssText = "padding-right: 10px";
+      row.appendChild(symbolCell);
+
+      // Description
+      var descCell = document.createElement("td");
+      if (!this.config.showFullName) {
+        descCell.innerHTML = this.stationData[i].tripSign;
+      } else {
+        descCell.innerHTML = this.stationData[i].tripName;
+      }
+      descCell.className = "align-left bright";
+      row.appendChild(descCell);
+
+      // ETA
+      if (this.config.showETATime) {
+        var preAwayCell = document.createElement("td");
+        var preAwayTime = parseInt(this.stationData[i].preAway);
+        if (preAwayTime < 10) { // Better to display single digits as "now"
+          preAwayCell.innerHTML = "Now";
+        } else {
+          var minutes = Math.floor(preAwayTime / 60);
+          var seconds = preAwayTime % 60;
+
+          if (this.config.showMinutesOnly) {
+            if (minutes === 0) {
+              preAwayCell.innerHTML = "< 1";
+            } else {
+              preAwayCell.innerHTML = minutes;
+            }
+          } else if (this.config.formatETA) { // Parses the time away into MM:SS
+            // Padding so we don't get something like 4:3 minutes...
+            if (seconds < 10) {
+              // lol what even is type casting
+              seconds = "0" + seconds;
+            }
+
+            preAwayCell.innerHTML = minutes + ":" + seconds;
+          } else {
+            preAwayCell.innerHTML = seconds;
+          }
+        }
+        row.appendChild(preAwayCell);
+      }
+
+      if (this.config.showArrivalTime) {
+        // Arrival time
+        var arrTimeCell = document.createElement("td");
+        if (config.timeFormat === 24) {
+          arrTimeCell.innerHTML = moment(parseInt(this.stationData[i].preDt) * 1000).format("H:mm");
+        } else {
+          arrTimeCell.innerHTML = moment(parseInt(this.stationData[i].preDt) * 1000).format("h:mm");
+        }
+        row.appendChild(arrTimeCell);
+      }
+
+      // Stolen from default modules to ensure style is identical. Thanks MichMich <3
+      if (this.config.fade && this.config.fadePoint < 1) {
+        if (this.config.fadePoint < 0) {
+          this.config.fadePoint = 0;
+        }
+        var startingPoint = this.stationData.length * this.config.fadePoint;
+        var steps = this.stationData.length - startingPoint;
+        if (i >= startingPoint) {
+          var currentStep = i - startingPoint;
+          row.style.opacity = 1 - (1 / steps * currentStep);
+        }
+      }
+    }
+
+    wrapper.appendChild(table);
+
+    // Don't start the update loop on first init
+    if (this.loaded) {
+      this.scheduleUpdate();
+    }
+
+    if (this.config.showAlerts) {
+      // This might break in future updates
+      var alertHeader = document.createElement("header");
+      alertHeader.className = "module-header";
+      alertHeader.innerHTML = "Alerts";
+      wrapper.appendChild(alertHeader);
+
+      var alertTable = document.createElement("table");
+      alertTable.className = "small";
+
+      for (let i = 0; i < this.alerts.length; i++) {
+        var row = document.createElement("tr");
+        alertTable.appendChild(row);
+        alertTable.style.cssText = "width: inherit";
+
+        // Icon
+        var symbolCell = document.createElement("td");
+        switch (this.alerts[i].effect_name) {
+          case "Delay":
+            symbolCell.className = "fa fa-exclamation-triangle";
+            break;
+          case "Service Change":
+            symbolCell.className = "fa fa-exclamation-circle";
+            break;
+          default:
+            symbolCell.className = "fa fa-question-circle-o";
+        }
+        symbolCell.style.cssText = "padding-right: 10px";
+        row.appendChild(symbolCell);
+
+        // If I ever want to add in marquee effect https://stackoverflow.com/a/21233577/5054366
+        var descCell = document.createElement("td");
+        descCell.innerHTML = this.alerts[i].header_text;
+        descCell.className = "light small";
+        row.appendChild(descCell);
+
+      }
+
+      wrapper.appendChild(alertTable);
+    }
+
+
+    return wrapper;
+  },
+
+  notificationReceived: function (notification, payload, sender) {
+    if (notification === "DOM_OBJECTS_CREATED") {
+      Log.log(this.name + " received a system notification: " + notification);
+      this.fetchData(true);
+      Log.log("updating dom");
+    }
+
+  },
+
+  // Note to self: This is called by getDom(), which this eventually calls...
+  // Shouldn't be a problem... right?
+  scheduleUpdate: function (amt) {
+    var interval;
+    if (amt !== undefined) {
+      interval = amt;
+    } else {
+      interval = this.config.updateInterval;
+    }
+
+    var self = this;
+
+    setTimeout(function () {
+      self.fetchData();
+      if (self.config.doAnimation) {
+        self.updateDom(self.config.animationSpeed);
+      } else {
+        self.updateDom();
+      }
+    }, interval * 1000);
+  },
+
+  // params: updateDomAfter: boolean, whether or not to call updateDom() after processing data.
+  fetchData: function (updateDomAfter) {
+    for (let stop in this.stations) {
+      var url = this.formUrl(this.stations[stop]);
+      var MBTARequest = new XMLHttpRequest();
+      MBTARequest.open("GET", url, true);
+
+      var self = this;
+      MBTARequest.onreadystatechange = function () {
+        if (this.readyState === 4) {
+          if (this.status === 200) {
+            self.processData(JSON.parse(this.response), updateDomAfter);
+          }
+        }
+      };
+
+      MBTARequest.send();
+    }
+  },
+
+  // Gets API URL based off user settings
+  formUrl: function (stopId) {
+    var url = this.config.baseUrl;
+
+    // query goes here
+    url += "predictionsbystop";
+
+    url += "?api_key=" + this.config.apikey;
+    url += "&format=json";
+    url += "&stop=" + stopId;
+
+    return url;
+  },
+
+  // updateDomAFter: immediatelly call updateDom() if true
+  processData: function (data, updateDomAfter) {
+    /* Nice little list of everything we have
+    Each element in this array is an entry on our displayed table.
+    Format should be
+    {"routeType": int,
+     "dirName": string,
+     "tripName": string,
+     "tripSign": string,
+     "preDt": int,
+     "preAway": int}
+     */
+
+    this.stationData = []; // clear all data.
+
+    // Please, if you know how to simplify this and make this readable, I would love you forever.
+    var curDir;
+    var rawData = [];
+    for (let mode = 0; mode < data["mode"].length; mode++) {
+      curDir = data["mode"][mode];
+      var routeType = curDir.route_type;
+
+      curDir = curDir["route"][0]["direction"];
+      for (let direction = 0; direction < curDir.length; direction++) {
+        var dirName = curDir[direction].direction_name;
+
+        for (let trip = 0; trip < curDir[direction]["trip"].length; trip++) {
+          var tripName = curDir[direction]["trip"][trip].trip_name,
+            tripSign = curDir[direction]["trip"][trip].trip_headsign,
+            preDt = curDir[direction]["trip"][trip].pre_dt;
+          preAway = curDir[direction]["trip"][trip].pre_away;
+          rawData.push({
+            // Note all values are strings. I'd really like to not worry about
+            // loose js casting.
+            routeType: routeType,
+            dirName: dirName,
+            tripName: tripName,
+            tripSign: tripSign,
+            preDt: preDt,
+            preAway: preAway
+          });
+        }
+      }
+    }
+
+    // Filters out items
+    // This simply doesn't run when the param is empty.
+    var self = this;
+    for (let i = 0; i < this.filterModes.length; i++) {
+      var temp = rawData.filter(obj => (obj.routeType === self.filterModes[i]));
+
+      // For some reason a for-each loop won't work.
+      for (let x = 0; x < temp.length; x++) {
+        this.stationData.push(temp[x]);
+      }
+    }
+
+    if (this.filterModes.length === 0) {
+      this.stationData = rawData;
+    }
+
+    // Sorts them according to ETA time
+    this.stationData.sort((a, b) => (a.preDt - b.preDt));
+
+    if (this.config.maxTime !== 0) {
+      this.stationData = this.stationData.filter(obj => (obj.preAway <= this.config.maxTime * 60));
+    }
+
+    // Shortens the array
+    if (this.stationData.length > this.config.maxEntries) {
+      this.stationData.length = this.config.maxEntries;
+    }
+
+    /*-----------------------------*/
+
+    if (this.config.showAlerts) {
+      this.alerts = [];
+      for (let i = 0; i < data["alert_headers"].length; i++) {
+        this.alerts.push(data["alert_headers"][i]);
+      }
+    }
+
+    this.loaded = true;
+
+    if (updateDomAfter) {
+      this.updateDom();
+    }
+  },
+});
---
+++ modules/MMM-MBTA/readme.md
@@ -0,0 +1,94 @@
+
+# MMM-MBTA
+
+![Example of MMM-MBTA](./example_picture.png)
+
+This [MagicMirror²][mm] Module is designed to pull information from the Massachusetts Bay Transportation Authority's API and to show and filter data.
+
+For this to work, you need to create your own MBTA API key. You can get one by following [this link][mbta dev portal] and 
+signing up. Note that it may take up to a day to approve your api key, according to their website.
+
+
+**DISCLAIMER**
+
+The times provided are from MBTA's API. As a result, the predicted values may be off. Please only use this as an estimation. i am not responsible for missed transportation and any consequences as a result of it. This is not a precise tool, nor have I claimed it to be. By using this module, you agree to never assume that this is a precise tool, and that I take no fault in any missed appointments, finals, etc.
+
+## Installation
+
+In your terminal, go to your MagicMirror's Module folder:
+
+```bash
+cd ~/MagicMirror/modules
+```
+Clone this repository:
+```bash
+git clone https://github.com/edward-shen/MMM-MBTA.git
+```
+Configure the module in your config.js file.
+
+## Using the module
+
+To use this module, add it to the modules array in the `config/config.js` file:
+```js
+modules: [
+    {
+        module: 'MMM-MBTA',
+        position: 'top_right', // This can be any of the regions.
+        header: "MBTA:",
+        config: {
+            apikey: 'your_api_key',
+        }
+    }
+]
+```
+
+## Configuration options
+
+Option|Description
+------|-----------
+`apikey`|Api key given by the MBTA.<br/>**Expected Value type:** `String`.
+`updateInterval`|Time between updates, in seconds. To protect API abuse, this module automatically limits this value to a minimum of `10`.<br/>**Expected Value type:** `int`.
+`baseUrl`|The base url of the MBTA api. You shouldn't change this unless the MBTA moved the api to the new location, and the update hasn't been reflected in this module.<br/>**Expected Value type:** `String`.
+`stations`|This is an array of stations that you wish to display. Currently, only one max station is allowed. For a list of station names, please refer to `stations-formatted.json`. Make sure to use the common name!<br/>Example: `stations: [ "Airport" ]`.<br/>**Expected Value type:** `[String, String, ...]`.
+`doAnimation`|Whether or not to do an animation when updating.<br/>**Expected Value type:** `boolean`.
+`animationSpeed`|How long the update animation lasts. <br/>**Expected Value type:** `int`.
+`formatETA`|Show ETA time in MM:SS instead of just seconds.<br/>**Expected Value type:** `boolean`.
+`showMinutesOnly`|Show ETA time in minutes only. This overrides `formatETA`.<br/>**Expected Value type:** `boolean`.
+`showOnly`|Filter out various modes of transportation. Example: `showOnly: ["Subway"]` would only show subways, while `showOnly: ["Subway", "Bus"]` would show both subways and buses. Default is to show all modes of transportation.<br/>**Expected Value type:** `[String, String, ...]`.<br/>The various options are `Subway`, `Train`, `Bus`, `Ferry`, `Cable car`.
+`maxEntries`|The maximum entries allowed. Default is `8`.<br/>**Expected Value type:** `int`.
+`maxTime`|The maximum ETA time the modules should list, in minutes. Default is to show all. <br/>**Expected Value type:** `int`.
+`showArrivalTime`|Show MBTA's predicted arrival time of the transportation vehicle.<br/>**Expected Value type:** `boolean`.
+`showETATime`|Show MBTA's predicted ETA for the vehicle.<br/>**Expected Value type:** `boolean`.
+`fade`|Whether or not to fade the module.<br/>**Expected Value type:** `boolean`.
+`fadePoint`|At what point to start fading. <br/>**Expected Value type:** `float`. Value range is from 0 to 1.
+`showFullName`|Display the full name instead of the short name. Due to the length the text, it is strongly recommended that the postition of this module is set to `top_bar`, `bottom_bar`, or any location where it can expand to the entire width when this is set to `true`.<br/>**Expected Value type:** `boolean`.
+`showAlerts`|Shows alerts near the specified station.<br/>**Expected Value type:** `boolean`.<br/>**WARNING**: Alerts are only properly shown when in a full-width position, such as `top_bar`, `middle_center` or `lower_third`.
+
+More options will be added as this module becomes feature-rich.
+
+## Planned Features
+- [x] Options to display minutes and seconds vesus just seconds
+- [x] Options to display full description name
+- [x] Options to display arrival time
+- [x] Options to filter various modes of transportation
+- [x] Formatting that matches the default modules
+- [x] Alert tickers **Partial support**
+- [x] Fade effect
+- [x] Animations
+- [x] Cap on maximum amount of vehicles displayed
+
+This list was last updated on 2017-06-13.
+
+## Known Bugs
+- Gondola and funicular icons are unsupported!
+
+  This is intentional. Unless there's a Font Awesome icon, or one that resembles one, I'm not adding it in, because there shouldn't be gondolas and funiculars in Boston anyways.
+
+- The header disappears when you fade the rest out! What gives?
+
+  Unfortunately, that's due to an issue with MagicMirror² core. I may do a pull request in the future, but as of right now, this is (unfortunate) intented behavior.
+
+This list was last updated on 2017-06-13.
+
+[mm]: https://github.com/MichMich/MagicMirror
+[mbta dev portal]: http://realtime.mbta.com/Portal/
