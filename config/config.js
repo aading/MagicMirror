a3e5075ed9fc796e3bed45a6783f5e100e32d6cd
Ding, Haochuan <Haochuan.Ding@Kronos.com>
2019-09-12 17:39:13 -0400

    revert xiaomi settings

--- config/config.js
+++ config/config.js
@@ -91,6 +91,114 @@ var config = {
       }
     },
     {
+      module: 'MMM-xiaomi',
+      position: 'top_left',
+      header: 'Temperature / Humidity',  // This is optional
+      config: {
+        gatewayIP: '192.168.1.75',
+        gatewayToken: '38f23cda57f2a22963338f03aa4d0b59',
+        outsideSensorId: '158d000223f705',
+        showWindow: true,
+        celcius: false,
+        showVentilation: true,
+        showLights: false,
+        audioNotifications: false,
+        showTrend: true,
+        rooms: [
+          {
+            name: 'Outside',
+            sortOrder: 1,
+            devices: ['158d000223f705']
+          },
+          {
+            name: 'Master',
+            sortOrder: 2,
+            devices: ['158d0001f2abe2']
+          },
+          {
+            name: 'Kayley',
+            sortOrder: 3,
+            devices: ['158d00019cbad8']
+          },
+          {
+            name: 'Guest',
+            sortOrder: 4,
+            devices: ['158d0001c1578c']
+          },
+          {
+            name: 'Grandparents',
+            sortOrder: 5,
+            devices: ['158d000223fca1']
+          },
+          {
+            name: 'Kitchen',
+            sortOrder: 6,
+            devices: ['158d00016ffb90']
+          },
+          {
+            name: 'Living',
+            sortOrder: 7,
+            devices: ['158d0001f4effe']
+          },
+          {
+            name: 'Family',
+            sortOrder: 8,
+            devices: ['158d000223f56e']
+          },
+          {
+            name: 'Garage',
+            sortOrder: 9,
+            devices: ['158d0001f515ec']
+          }
+        ]
+      }
+    },
+    // {
+    //   module: 'MMM-Scrobbler',
+    //   position: 'top_right',
+    //   config: {
+    //     username: 'aarondkk',
+    //     apikey: '99deb2c2033ae44616de4cbd7d7d7e84',
+    //     //time interval to search for new song (every 15 seconds)
+    //     updateInterval: 15 * 1000,
+    //     //how often should we try to retrieve a song if not listening
+    //     delayCount: 5,
+    //     //time interval to search for new song if the 5 times not listening is received.
+    //     //set this to the same number as updateInterval to ignore this option
+    //     delayInterval: 120*1000,
+    //     animationSpeed: 1000,
+    //     showAlbumArt: true,
+    //     showMetaData: true,
+    //     //Determines the position of the meta text. Possible values: top, bottom, left, right
+    //     alignment: "bottom",
+    //   }
+    // },
+    {
+      module: 'MMM-Trello',
+      header: 'Home Reminder',
+      position: 'top_right', // This can be any of the regions, best results in center regions.
+      config: {
+        // See 'Configuration options' for more information.
+        api_key: "18483816fc662cc173f0a69bd0513a1c",
+        token: "2f373e422105b4e923559b538190063441d588d09da980a0613a5ae2378ca613",
+        list: "5a240991f714c6bf237857c3",
+        wholeList: 'true',
+      }
+    },
+    {
+      module: 'MMM-GoogleMapsTraffic',
+      position: 'top_left',
+      header: 'Traffic Info',
+      config: {
+        key: 'AIzaSyC1RkVgwzb3fj8B0R0w6wLXRXwPcpBBG1Y',
+        lat: 42.44481,
+        lng: -71.1718486,
+        zoom: 12,
+        height: '600px',
+        width: '400px'
+      }
+    },
+    {
       module: "newsfeed",
       position: "bottom_bar",
       config: {
