dfccfbf1ccf1384ed0209837faa9f9555793ce77
Ding, Haochuan <Haochuan.Ding@Kronos.com>
2020-05-09 09:04:18 -0400

    Disable notification for xiaomi

--- config/config.js
+++ config/config.js
@@ -79,6 +79,7 @@ var config = {
         showWindow: true,
         celcius: false,
         showVentilation: true,
+        showNotifications: false,
         showLights: false,
         audioNotifications: false,
         showTrend: true,
